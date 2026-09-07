4d27dbf9e9a38aa708aac9ab88d8b06f2ee70a96
berdiyev-english <abdurrahimberdiev@gmail.com>
2026-09-07 18:54:38 +0300

    работаем

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 /* Bewords Service Worker - Offline Mode */
-const CACHE_NAME = 'bewords-cache-v18';
+const CACHE_NAME = 'bewords-cache-v19';
 
 // Список файлов, которые нужно сохранить СРАЗУ
 const STATIC_ASSETS = [
